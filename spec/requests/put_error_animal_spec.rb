e995ad0b577cc6f0973d84fc4c0d8204016687d5
nhhor <nhhor@post.com>
2020-01-31 15:47:09 -0800

    add PUT_error tests in place. 16 of 16 tests passing.

--- spec/requests/put_error_animal_spec.rb
+++ spec/requests/put_error_animal_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-describe "put to an animal route", :type => :request do
+describe "put to an animal route error handling", :type => :request do
   let!(:animals) { FactoryBot.create_list(:animal, 3)}
 
   before { get "/animals/#{Animal.all.first.id}" }
 
-  it 'updates correct information for animal at specific id' do
+  it 'updates incorrect information for animal at specific id' do
     # p 'OK'
     # p "RSPEC: PUT_animal_spec: on /animals/#{Animal.all.first.id}, a #{Animal.all.first.animal_type} named #{Animal.all.first.name}."
-    put "/animals/#{Animal.all.first.id}", params: {:animal_type => "Frog"}
+    put "/animals/#{Animal.all.first.id}", params: {:name => "", :animal_type => "Frog", :birthday => "1-1-99"}
     # p "RSPEC: PUT_animal_spec: on /animals/#{Animal.all.first.id}, a #{Animal.all.first.animal_type} named #{Animal.all.first.name}."
-    expect(JSON.parse(response.body)['message']).to eq("This animal has been updated successfully.")
+    expect(JSON.parse(response.body)['message']).to eq("Validation failed: Name can't be blank, Birthday only allows numbers in yyyy-mm-dd format")
   end
 
-  it 'returns status code 200' do
+  it 'returns status code 404' do
     expect(response).to have_http_status(:success)
   end
 end
