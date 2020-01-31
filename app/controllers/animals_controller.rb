e2ae8e523dbdb7cb6255c41ae3c866fee875321c
nhhor <nhhor@post.com>
2020-01-31 11:06:15 -0800

    move JSON response to its own controllers-concern.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -26,9 +26,6 @@ class AnimalsController < ApplicationController
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
 
   def animal_params
     params.permit(:author, :content)
